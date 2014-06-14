f54b519046f016e2d15a0f257d6d6d88c02acbcb
Orleika <Orleika.net@gmail.com>
2014-06-15 04:55:41 +0900

    ver3.2

--- gallery/index.php
+++ gallery/index.php
@@ -18,7 +18,63 @@
       <?php include_once("../_parts/side_nav.html"); ?>
 
       <!--メイン-->
-      <div class="col-md-8"></div>
+      <div class="col-md-8">
+        <div id="gif-anime" class="row">
+          <div class="col-md-6 col-md-offset-3">
+            <a href="#" class="thumbnail">
+              <img src="./img/thumbnail-sample.png" alt="thumbnail-sample">
+            </a>
+          </div>
+        </div>
+
+        <!--サムネイル-->
+        <div id="gallery">
+          <div class="row">
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+            <div class="col-md-4">
+              <a href="#" class="thumbnail">
+                <img src="./img/thumbnail-sample.png" alt="thumbnail-sample"></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <hr>
@@ -30,24 +86,31 @@
 
   <?php include_once("../_parts/script.html"); ?>
   <script src="js/querystring.js"></script>
+  <script src="js/gallery.js"></script>
   <script>
   $(function () {
-    /*if (!query.id) {
+    if (!query.id) {
       location.href = 'new/';
-    }*/
+    }
 
-    $('#side-nav li').eq(3).attr({'class': 'active'});
+    var url = '//tokyo-animation.azurewebsites.net/api/animations/' + query['id'],
+      getGif = function (json) {
+        $('#gif-anime img').attr({'src': json.Url, 'alt': 'gif animation'});
+      };
 
-    /*$.ajax({
+    $('#gallery').thumGallery({'url': url});
+
+    $.ajax({
       async: true,
-      url: '//tokyo-animation.azurewebsites.net/api/animations/' + query['id'],
+      url: url,
       type: 'GET',
       dataType: 'json'
     }).done(function (json) {
-      getGallery(json.Pictures);
+      getGif(json);
     }).fail(function () {
     }).always(function () {
-    });*/
+    });
   });
-  </script></body>
+  </script>
+</body>
 </html>
